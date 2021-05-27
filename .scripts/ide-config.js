7934d8652625e55205ecd6e14f326339c0feffbd
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-05-28 00:09:10 +0300

    refactor: difference between node-gyp and cmake-js

---
+++ .scripts/ide-config.js
@@ -0,0 +1,128 @@
+const {spawn} = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+function runConfigure(tool, callback) {
+  console.log(`Running '${tool}' configuration...`);
+  const npx = spawn('npx', [tool, 'configure']);
+  let config = '';
+
+  npx.stdout.on('data', (data) => {
+    config = `${config}${data}`;
+  });
+
+  npx.stderr.on('data', (data) => {
+    config = `${config}${data}`;
+  });
+
+  npx.on('close', (code) => {
+    if (code !== 0) {
+      console.error(config);
+      process.exit(code);
+    }
+    console.log('Done.');
+    callback();
+  });
+}
+
+function parseCMakeJsSettingsAndConfigureIde(callback) {
+  console.log('Reading configuration...');
+
+  let includes = [];
+
+  // if (process.platform !== 'win32') {
+  const cMakeCachePath = path.join(__dirname, '..', 'build', 'CMakeCache.txt');
+  const matches = fs
+    .readFileSync(cMakeCachePath)
+    .toString()
+    .match(/CMAKE_JS_INC:UNINITIALIZED=.+/gi);
+
+  if (Array.isArray(matches) && matches.length > 0) {
+    includes = matches[0].split('=')[1].split(';');
+  } else {
+    console.error('Could not read CMakeCache.txt. No included discovered');
+    process.exit(1);
+  }
+  // } else {
+  //   // TODO:
+  // }
+
+  console.log('Done.');
+
+  callback(includes);
+}
+
+function parseNodeGypSettingsAndConfigureIde(callback) {
+  console.log('Reading configuration...');
+  let includes = [];
+  if (process.platform !== 'win32') {
+    includes = parseMainTargetMk();
+  } else {
+    // TODO:
+  }
+  console.log('Done.');
+
+  callback(includes);
+}
+
+function parseMainTargetMk() {
+  const incsRelease = fs
+    .readFileSync(path.join(__dirname, '..', 'build', 'main.target.mk'))
+    .toString()
+    .match(/INCS_Release := \\\n(\s+\-I[^\n]+\n)+/g);
+
+  if (!Array.isArray(incsRelease) || incsRelease.length === 0) {
+    console.error('Could not determine include list');
+    process.exit(1);
+  }
+
+  const localSrc = path.join(__dirname, '..', 'src');
+
+  return incsRelease[0]
+    .split('\n')
+    .slice(1)
+    .map((c) =>
+      c
+        .trim()
+        .replace(/^\-I/gi, '')
+        .replace(/\s+\\$/, '')
+        .replace('$(srcdir)', path.join(__dirname, '..')),
+    )
+    .filter((c) => c)
+    .filter((c) => c != localSrc)
+    .filter((c) => {
+      try {
+        return fs.statSync(c).isDirectory();
+      } catch (e) {
+        return null;
+      }
+    });
+}
+
+function writeCMakeListsTxt(includes) {
+  console.log('Configuring CMakeLists.txt ...');
+
+  const cMakeListsTxtPath = path.join(__dirname, '..', 'CMakeLists.txt');
+  const cMakeListsTxt = fs.readFileSync(`${cMakeListsTxtPath}.template`).toString();
+
+  const cmakeLines = includes.map((item) => `  include_directories(${item})`);
+
+  fs.writeFileSync(
+    cMakeListsTxtPath,
+    cMakeListsTxt.replace(
+      /\s+# start clion-config here\n(.+\n)*\s+# end clion-config here/gi,
+      `\n  # start clion-config here
+${cmakeLines.join('\n')}
+  # end clion-config here`,
+    ),
+  );
+
+  console.log('Done.');
+}
+
+module.exports = {
+  parseCMakeJsSettingsAndConfigureIde,
+  parseNodeGypSettingsAndConfigureIde,
+  runConfigure,
+  writeCMakeListsTxt,
+};
--- .scripts/vscode-config.js
+++ .scripts/vscode-config.js
@@ -1,89 +1,38 @@
 const fs = require('fs');
 const path = require('path');
-const {spawn} = require('child_process');
 
-runNodeGypConfigure(parseNodeGypSettingsAndConfigureVscode);
-
-function runNodeGypConfigure(callback) {
-  console.log('Running configuration...');
-  const npx = spawn('npx', ['node-gyp', 'configure']);
-  let config = '';
-
-  npx.stdout.on('data', (data) => {
-    config = `${config}${data}`;
-  });
-
-  npx.stderr.on('data', (data) => {
-    config = `${config}${data}`;
-  });
-
-  npx.on('close', (code) => {
-    if (code !== 0) {
-      console.error(config);
-      process.exit(code);
-    }
-    console.log('Done.');
-    callback();
-  });
+if (process.argv.includes('-h') || process.argv.includes('--help')) {
+  console.log(`node vscode-config.js
+  Arguments:
+  --use-cmake-js will configure VSCode based on cmake-js (default is node-gyp)
+  --use-clangd will configure VSCode based on the Clangd extension (default is Microsoft C/C++)`);
+  process.exit(0);
 }
 
-function parseMainTargetMk() {
-  const incsRelease = fs
-    .readFileSync(path.join(__dirname, '..', 'build', 'main.target.mk'))
-    .toString()
-    .match(/INCS_Release := \\\n(\s+\-I[^\n]+\n)+/g);
+const {
+  parseCMakeJsSettingsAndConfigureIde,
+  parseNodeGypSettingsAndConfigureIde,
+  runConfigure,
+  writeCMakeListsTxt,
+} = require('./ide-config');
 
-  if (!Array.isArray(incsRelease) || incsRelease.length === 0) {
-    console.error('Could not determine include list');
-    process.exit(1);
-  }
-
-  const localSrc = path.join(__dirname, '..', 'src');
-
-  return incsRelease[0]
-    .split('\n')
-    .slice(1)
-    .map((c) =>
-      c
-        .trim()
-        .replace(/^\-I/gi, '')
-        .replace(/\s+\\$/, '')
-        .replace('$(srcdir)', path.join(__dirname, '..')),
-    )
-    .filter((c) => c)
-    .filter((c) => c != localSrc)
-    .filter((c) => {
-      try {
-        return fs.statSync(c).isDirectory();
-      } catch (e) {
-        return null;
-      }
-    });
-}
-
-function parseNodeGypSettingsAndConfigureVscode() {
-  console.log('Reading configuration...');
-  let includes = [];
-  if (process.platform !== 'win32') {
-    includes = parseMainTargetMk();
-  } else {
-    // TODO:
-  }
-  console.log('Done.');
-
-  writeVSCodeConfig(includes);
+if (process.argv.includes('--use-cmake-js')) {
+  runConfigure('cmake-js', () => parseCMakeJsSettingsAndConfigureIde(writeVSCodeConfig));
+} else {
+  runConfigure('node-gyp', () => parseNodeGypSettingsAndConfigureIde(writeVSCodeConfig));
 }
 
 function writeVSCodeConfig(includes) {
   writeCCppProperties(includes);
   writeCompileFlagsTxt(includes);
+  writeCMakeListsTxt(includes);
   changeVsCodeSettings();
 }
 
 function writeCCppProperties(includes) {
   console.log('Configuring `ms-vscode.cpptools` extension...');
   const cppConfigPath = './.vscode/c_cpp_properties.json';
-  const cppConfig = JSON.parse(fs.readFileSync(cppConfigPath).toString());
+  const cppConfig = JSON.parse(fs.readFileSync(`${cppConfigPath}.template`).toString());
   cppConfig.configurations = cppConfig.configurations.map((itemConfig) => ({
     ...itemConfig,
     includePath: includes,
@@ -95,7 +44,7 @@ function writeCCppProperties(includes) {
 function writeCompileFlagsTxt(includes) {
   console.log('Configuring `llvm-vs-code-extensions.vscode-clangd` extension...');
   const compileFlagsTxtPath = path.join(__dirname, '..', 'compile_flags.txt');
-  const compileFlagsTxt = fs.readFileSync(compileFlagsTxtPath).toString();
+  const compileFlagsTxt = fs.readFileSync(`${compileFlagsTxtPath}.template`).toString();
   let lines = [];
   includes.forEach((c) => (lines = [...lines, '-I', c]));
 
@@ -114,19 +63,19 @@ ${lines.join('\n')}
 function changeVsCodeSettings() {
   console.log('Configuring .vscode/settings.json');
   const vscodeConfigPath = path.join(__dirname, '..', '.vscode', 'settings.json');
-  const vscodeConfig = JSON.parse(fs.readFileSync(vscodeConfigPath).toString());
+  const vscodeConfig = JSON.parse(fs.readFileSync(`${vscodeConfigPath}.template`).toString());
 
   vscodeConfig['[cpp]'] = {
     ...vscodeConfig['[cpp]'],
     'editor.defaultFormatter': 'llvm-vs-code-extensions.vscode-clangd',
   };
 
-  if (process.argv.includes('--clangd')) {
+  if (process.argv.includes('--use-clangd')) {
     console.log(`It seems you want to use clang for intellisense. Disabling C++ InstelliSenseEngine`);
     vscodeConfig['C_Cpp.intelliSenseEngine'] = 'Disabled';
   } else {
     console.log(
-      `It seems you want to use Microsoft C++ module. To use 'clangd', please add --clangd flag to the command.`,
+      `It seems you want to use Microsoft C++ module. To use 'clangd', please add --use-clangd flag to the command.`,
     );
     console.log(
       `If you wish to continue with the Microsoft C++ module, you should disable 'llvm-vs-code-extensions.vscode-clangd' module, in order to run things smoothly.`,
